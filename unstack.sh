dde5135a10451bdb5e6d5a3b6e0f870d8b4da1cd
Vincent Hou <sbhou@cn.ibm.com>
2012-06-28 14:34:09 +0800

    Add the function of stopping a specific service to unstack.sh.
    Fixes Bug1015535.
    
    Change-Id: Idc9578b0e7f66efea76c80c2a1e28763d5542c4c

--- unstack.sh
+++ unstack.sh
@@ -6,6 +6,11 @@
 #
 # Stop all processes by setting UNSTACK_ALL or specifying ``--all``
 # on the command line
+#
+# Stop a specific process by specifying ``-s $SERVICE_SHUT_DOWN`` or
+# ``--service $SERVICE_SHUT_DOWN`` on the command line.
+# For example, unstack.sh -s g-reg.
+
 
 # Keep track of the current devstack directory.
 TOP_DIR=$(cd $(dirname "$0") && pwd)
@@ -20,44 +25,105 @@ source $TOP_DIR/stackrc
 # ``os_RELEASE``, ``os_UPDATE``, ``os_PACKAGE``, ``os_CODENAME``
 GetOSVersion
 
-if [[ "$1" == "--all" ]]; then
+# Function to shut down all the services.
+function shutdown_all_services {
+    # Shut down devstack's screen to get the bulk of OpenStack services in one shot
+    SCREEN=$(which screen)
+    if [[ -n "$SCREEN" ]]; then
+        SESSION=$(screen -ls | awk '/[0-9].stack/ { print $1 }')
+        if [[ -n "$SESSION" ]]; then
+            screen -X -S $SESSION quit
+        fi
+    fi
+
+    # Swift runs daemons
+    if is_service_enabled swift; then
+        swift-init all stop
+    fi
+
+    # Apache has the WSGI processes
+    if is_service_enabled horizon; then
+        stop_service apache2
+    fi
+
+    # Get the iSCSI volumes
+    if is_service_enabled cinder n-vol; then
+        TARGETS=$(sudo tgtadm --op show --mode target)
+        if [[ -n "$TARGETS" ]]; then
+            # FIXME(dtroyer): this could very well require more here to
+            #                 clean up left-over volumes
+            echo "iSCSI target cleanup needed:"
+            echo "$TARGETS"
+        fi
+        stop_service tgt
+    fi
+
+    if [[ -n "$UNSTACK_ALL" ]]; then
+        # Stop MySQL server
+        if is_service_enabled mysql; then
+            stop_service mysql
+        fi
+    fi
+}
+
+#Declare the parameters for getopt
+declare unstackOptions
+declare -r ScriptName=${0##*/}
+declare -r ShortOpts="as:"
+declare -r LongOpts="all,service:"
+
+# Parse the parameters.
+# Execute getopt.
+unstackOptions=$(getopt -o "${ShortOpts}" --long \
+  "${LongOpts}" --name "${ScriptName}" -- "${@}")
+
+#No argument input.
+if [[ ($# -eq 0) ]]; then
     UNSTACK_ALL=${UNSTACK_ALL:-1}
+    shutdown_all_services
+    exit 0
 fi
 
-# Shut down devstack's screen to get the bulk of OpenStack services in one shot
-SCREEN=$(which screen)
-if [[ -n "$SCREEN" ]]; then
-    SESSION=$(screen -ls | awk '/[0-9].stack/ { print $1 }')
-    if [[ -n "$SESSION" ]]; then
-        screen -X -S $SESSION quit
-    fi
-fi
+#Preserve whitespaces inside options arguments.
+eval set -- "$unstackOptions"
 
-# Swift runs daemons
-if is_service_enabled swift; then
-    swift-init all stop
-fi
+while true; do
+  case "${1}" in
+    -a | --all)
+      UNSTACK_ALL=${UNSTACK_ALL:-1}
+      shutdown_all_services
+      exit 0
+      ;;
 
-# Apache has the WSGI processes
-if is_service_enabled horizon; then
-    stop_service apache2
-fi
+    -s | -service)
+      SERVICE_SHUT_DOWN="$2"
+      ;;
 
-# Get the iSCSI volumes
-if is_service_enabled cinder n-vol; then
-    TARGETS=$(sudo tgtadm --op show --mode target)
-    if [[ -n "$TARGETS" ]]; then
-        # FIXME(dtroyer): this could very well require more here to
-        #                 clean up left-over volumes
-        echo "iSCSI target cleanup needed:"
-        echo "$TARGETS"
-    fi
-    stop_service tgt
-fi
+    --)
+      break
+      ;;
 
-if [[ -n "$UNSTACK_ALL" ]]; then
-    # Stop MySQL server
-    if is_service_enabled mysql; then
-        stop_service mysql
+  esac
+  shift
+done
+shift
+
+# If -s SERVICE_SHUT_DOWN is specified, just shut down the specific service.
+# If -s SERVICE_SHUT_DOWN is not specified, shut down all the services.
+
+if [[ -n "$SERVICE_SHUT_DOWN" ]]; then
+    # Check if the service in available in ENABLED_SERVICE
+    if is_service_enabled $SERVICE_SHUT_DOWN; then
+        SCREEN=$(which screen)
+        if [[ -n "$SCREEN" ]]; then
+            SESSION=$(screen -ls | awk '/[0-9].stack/ { print $1 }')
+            if [[ -n "$SESSION" ]]; then
+                screen -S $SESSION -p $SERVICE_SHUT_DOWN -X stuff $'\003'
+                sleep 1
+                screen -S $SESSION -p $SERVICE_SHUT_DOWN -X stuff $'\004'
+            fi
+        fi
+    else
+        echo "$SERVICE_SHUT_DOWN is unable to shut down, because it is not running or it is not a valid service name."
     fi
 fi
