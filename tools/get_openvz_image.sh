d200c61a3ba5aa22b6551b43478fd9fdf364ae74
Devananda van der Veen <devananda.vdv@gmail.com>
2012-07-06 17:49:12 -0700

    enable testing of openvz guests
    This patch adds a new script which downloads and imports an openvz template,
    modifies stack.sh to recognize a new VIRT_TYPE option,
    and makes a few changes to some tests so that some implicit
    defaults (such as the guest user account) can be overridden.
    
    Change-Id: I0dde2dffbf3848fac1dd27eb37af84c0ac73d9aa

---
+++ tools/get_openvz_image.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# **get_openvz_image.sh**
+
+# Download and prepare Ubuntu OpenVZ image
+
+CACHEDIR=${CACHEDIR:-/opt/stack/cache}
+
+# Keep track of the current directory
+TOOLS_DIR=$(cd $(dirname "$0") && pwd)
+TOP_DIR=$(cd $TOOLS_DIR/..; pwd)
+
+# Import configuration
+. $TOP_DIR/openrc
+
+# Exit on error to stop unexpected errors
+set -o errexit
+set -o xtrace
+
+usage() {
+   echo "Usage: $0 - Download and prepare Ubuntu OpenVZ image"
+   echo ""
+   exit 1
+}
+
+# Clean up any resources that may be in use
+cleanup() {
+    set +o errexit
+
+    # Mop up temporary files
+    if [ -n "$IMG_FILE_TMP" -a -e "$IMG_FILE_TMP" ]; then
+        rm -f $IMG_FILE_TMP
+    fi
+
+    # Kill ourselves to signal any calling process
+    trap 2; kill -2 $$
+}
+
+while getopts hr: c; do
+    case $c in
+        h)  usage
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+IMG_FILE="ubuntu-11.10-x86_64.tar.gz"
+IMG_FILE_URL="http://download.openvz.org/template/precreated/$IMG_FILE"
+IMG_FILE_TMP=`mktemp $IMG_FILE.XXXXXX`
+
+trap cleanup SIGHUP SIGINT SIGTERM SIGQUIT EXIT
+
+# download the image
+wget -nv -O $IMG_FILE_TMP $IMG_FILE_URL
+
+# load the image into glance
+glance image-create --is-public 1 --disk-format ami --name vz-template-ubuntu-11_10 < $IMG_FILE_TMP
+rm -f $IMG_FILE_TMP
+
+trap - SIGHUP SIGINT SIGTERM SIGQUIT EXIT
