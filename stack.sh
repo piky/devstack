545db7297cb0d660767cff8c0f049180ce577eba
zhhuabj <zhhuabj@cn.ibm.com>
2012-07-26 15:01:04 +0800

    repair the bug about why the exercise.sh scripts have failures when Quantum is enabled
    bug 1014187
    
    ensure that devstack exercise scripts are ok when quantum is enabled.
    
    Patch Set 3: upgrade quantum api from v1 to v2
    
    Patch Set 4: change sleep to "until 120 second timeout is reached"
    
    Change-Id: Ie81eda568a7524a64bfb32a70f2cbbaaf13a8a10

--- stack.sh
+++ stack.sh
@@ -356,15 +356,17 @@ function read_password {
 # FIXME: more documentation about why these are important options.  Also
 # we should make sure we use the same variable names as the option names.
 
+FLAT_NETWORK_BRIDGE_DEFAULT=br100
+GUEST_INTERFACE_DEFAULT=eth0
 if [ "$VIRT_DRIVER" = 'xenserver' ]; then
     PUBLIC_INTERFACE_DEFAULT=eth3
     # allow build_domU.sh to specify the flat network bridge via kernel args
     FLAT_NETWORK_BRIDGE_DEFAULT=$(grep -o 'flat_network_bridge=[[:alnum:]]*' /proc/cmdline | cut -d= -f 2 | sort -u)
     GUEST_INTERFACE_DEFAULT=eth1
+elif is_service_enabled q-svc; then
+    PUBLIC_INTERFACE_DEFAULT=eth0
 else
     PUBLIC_INTERFACE_DEFAULT=br100
-    FLAT_NETWORK_BRIDGE_DEFAULT=br100
-    GUEST_INTERFACE_DEFAULT=eth0
 fi
 
 PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-$PUBLIC_INTERFACE_DEFAULT}
