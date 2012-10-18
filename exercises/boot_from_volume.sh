878a94670711c5fe97597e92a3857fd785666f3e
Mate Lakat <mate.lakat@citrix.com>
2012-10-18 13:55:28 +0100

    Download cirros from launchpad
    Fixes bug 1068110.
    
    images.ansolabs.com/cirros.. was not available, so this change modifies
    the download location to launchpad.
    
    Change-Id: Idbfebce4e797256cf87a8d5ad0f42c0b3efc7148

--- exercises/boot_from_volume.sh
+++ exercises/boot_from_volume.sh
@@ -169,7 +169,7 @@ EOF
 
 # Download cirros
 if [ ! -e cirros-0.3.0-x86_64-rootfs.img.gz ]; then
-    wget http://images.ansolabs.com/cirros-0.3.0-x86_64-rootfs.img.gz
+    wget https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-rootfs.img.gz
 fi
 
 # Copy cirros onto the volume
