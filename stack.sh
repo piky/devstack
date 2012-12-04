c8fefa9a70e9897881477d6a966add37852207de
Nikhil Manchanda <SlickNik@gmail.com>
2012-12-03 18:15:09 -0700

    Added Trove (Database as a Service).
    Integrate Trove into devstack
    - Added changes to stackrc for the Trove Repos.
    - Added support to devstack for "install", "configure",
      "init", and "run"
    
    Fixes blueprint:trove-devstack-integration
    
    Change-Id: Ib3f6daad33e629f764a174b80762c808ce8588e2

--- stack.sh
+++ stack.sh
@@ -2,8 +2,8 @@
 
 # ``stack.sh`` is an opinionated OpenStack developer installation.  It
 # installs and configures various combinations of **Ceilometer**, **Cinder**,
-# **Glance**, **Heat**, **Horizon**, **Keystone**, **Nova**, **Neutron**
-# and **Swift**.
+# **Glance**, **Heat**, **Horizon**, **Keystone**, **Nova**, **Neutron**,
+# **Swift**, and **Trove**
 
 # This script allows you to specify configuration options of what git
 # repositories to use, enabled services, network configuration and various
@@ -313,6 +313,7 @@ source $TOP_DIR/lib/heat
 source $TOP_DIR/lib/neutron
 source $TOP_DIR/lib/baremetal
 source $TOP_DIR/lib/ldap
+source $TOP_DIR/lib/trove
 
 # Set the destination directories for other OpenStack projects
 OPENSTACKCLIENT_DIR=$DEST/python-openstackclient
@@ -759,6 +760,14 @@ if is_service_enabled heat; then
     install_heat
     cleanup_heat
     configure_heat
+fi
+
+if is_service_enabled trove; then
+    install_trove
+    install_troveclient
+    cleanup_trove
+    configure_trove
+    configure_troveclient
 fi
 
 if is_service_enabled tls-proxy; then
@@ -1248,6 +1257,16 @@ if is_service_enabled heat; then
     start_heat
 fi
 
+# Configure and launch the trove service api, and taskmanager
+if is_service_enabled trove; then
+    # Initialize trove
+    echo_summary "Configuring Trove"
+    init_trove
+
+    # Start the trove API and trove taskmgr components
+    echo_summary "Starting Trove"
+    start_trove
+fi
 
 # Create account rc files
 # =======================
