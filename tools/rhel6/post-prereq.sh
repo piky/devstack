97b2f6975ca5d789f5e5f9a6248e500eac7acbe4
Ian Wienand <iwienand@redhat.com>
2013-04-11 11:14:20 +1000

    Add a RHEL6 post-prereq phase
    Add some post-prereq actions for RHEL6:
    
     * pre-install dbus to avoid the service not having ever been started
     * a workaround for a hgtools/setuptools bug
     * install pycrypto and lxml as pip dependencies to ensure overrding
       of local versions
     * fix-up a dependency issue with prettytable by pre-installing
    
    Change-Id: I089c07da32d211d25b14ce62cb61d2e3e8e6bb9f

---
+++ tools/rhel6/post-prereq.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# **rhel6 post-prereq**
+
+# Extra workarounds for rhel6
+
+# Pre-install dbus and restart; otherwise it may get dragged in as a
+# dependency and you may hit a bug where the uuid isn't generated
+# because the service was never started (PR#598200), causing issues
+# for Nova stopping later on complaining that
+# '/var/lib/dbus/machine-id' doesn't exist.
+install_package dbus
+sudo service messagebus restart
+
+# RH#924038; keyring fails without this pre-installed
+pip_install hgtools
+
+# python-crypto conflicts with required pycrypto
+uninstall_package python-crypto
+# and requires gcc to build...
+install_package gcc
+# pre-install it now
+pip_install pycrypto
+
+# remove local version & install pip version of lxml for nova which
+# wants >=2.3
+uninstall_package python-lxml
+sudo yum-builddep -y python-lxml
+pip_install lxml
+
+# many of the python-* openstack projects specify prettytable <0.7,
+# but some other pip installations may have unversioned prettytable
+# requirements; so if they install first then we end up with a later
+# verison which therefore causes dependency issues later.  Installing
+# the RHEL one gives up 0.6(something) which keeps everyone happy.
+install_package python-prettytable
+
+
+# Local variables:
+# mode: Shell-script
+# End:
