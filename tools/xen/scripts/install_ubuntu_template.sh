1f493f8a03311ecd3ece432fe1bb1cac9d7ea051
Mate Lakat <mate.lakat@citrix.com>
2013-05-07 16:58:17 +0100

    xenapi - cleanup
    Add error on unitialised variable to the scripts. This way some issues
    were identified. Also modify README with fresh variables. The patch
    includes:
     - Add SWIFT_HASH to the README
     - Add XENAPI_CONNECTION_URL to the README
     - Add VNCSERVER_PROXYCLIENT_ADDRESS to the README
     - Introduce UBUNTU_INST_IFACE which is the OpenStack VM interface used
       for the netinstall. It defaults to eth2. Previously this parameter
       was set by the combination of HOST_IP_IFACE (which defaults to eth3)
       and the undocumented NETINSTALL_IP
     - xenrc inscludes CLEAN_TEMPLATES
     - xenrc no longer tries to do stackrc or change directory
    
    Related to blueprint xenapi-devstack-cleanup
    
    Change-Id: Ie9a75321c7f41cc9a0cc051398d1e6ec2c88adfa

--- tools/xen/scripts/install_ubuntu_template.sh
+++ tools/xen/scripts/install_ubuntu_template.sh
@@ -7,10 +7,7 @@
 # Based on a script by: David Markey <david.markey@citrix.com>
 #
 
-# Exit on errors
-set -o errexit
-# Echo commands
-set -o xtrace
+set -exu
 
 # This directory
 BASE_DIR=$(cd $(dirname "$0") && pwd)
@@ -54,11 +51,11 @@ disk_size=$(($OSDOMU_VDI_GB * 1024 * 1024 * 1024))
 pvargs="-- quiet console=hvc0 partman/default_filesystem=ext3 \
 console-setup/ask_detect=false locale=${UBUNTU_INST_LOCALE} \
 keyboard-configuration/layoutcode=${UBUNTU_INST_KEYBOARD} \
-netcfg/choose_interface=${HOST_IP_IFACE} \
+netcfg/choose_interface=${UBUNTU_INST_IFACE} \
 netcfg/get_hostname=os netcfg/get_domain=os auto \
 url=${preseed_url}"
 
-if [ "$NETINSTALLIP" != "dhcp" ]; then
+if [ "$UBUNTU_INST_IP" != "dhcp" ]; then
     netcfgargs="netcfg/disable_autoconfig=true \
 netcfg/get_nameservers=${UBUNTU_INST_NAMESERVERS} \
 netcfg/get_ipaddress=${UBUNTU_INST_IP} \
