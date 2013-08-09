14bff511ceb8453405541c64bb4d63b192e68b2c
Dean Troyer <dtroyer@gmail.com>
2013-08-09 15:09:31 -0500

    More Python package fixes
    * Add tools/fixup_stuff.sh to fix prettytable install with pip 1.4+
    * Cache downloads properly in tools/install_pip.sh
    
    Change-Id: I482590cb91f7a10c1436bc9015afd572ac1cc73e

---
+++ tools/fixup_stuff.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# **fixup_stuff.sh**
+
+# fixup_stuff.sh
+#
+# All distro and package specific hacks go in here
+# - prettytable 0.7.2 permissions are 600 in the package and
+#   pip 1.4 doesn't fix it (1.3 did)
+
+# Keep track of the current directory
+TOOLS_DIR=$(cd $(dirname "$0") && pwd)
+TOP_DIR=`cd $TOOLS_DIR/..; pwd`
+
+# Change dir to top of devstack
+cd $TOP_DIR
+
+# Import common functions
+source $TOP_DIR/functions
+
+FILES=$TOP_DIR/files
+
+# PrettyTable
+# -----------
+
+# Pre-install prettytable so we can fix the permissions
+sudo pip install prettytable
+
+SITE_DIRS=$(python -c "import site; import os; print os.linesep.join(site.getsitepackages())")
+for dir in $SITE_DIRS; do
+
+    # Fix prettytable 0.7.2 permissions
+    if [[ -r $dir/prettytable.py ]]; then
+        sudo chmod +r $dir/prettytable-0.7.2*/*
+    fi
+
+done
