adf534d464b6a8e1b9d1ece7f7a5da034b1b9f9b
Shawn Hartsock <hartsocks@vmware.com>
2013-08-13 21:41:11 -0400

    vmware - vsphere v5.0 - WSDL cleanup script.
    fixes bug 1211853
    
    When using DevStack with vSphere 5.0, it may be
    necessary to locally cache the WSDL locally to
    the nova-compute driver for vCenter or ESXi.
    This will allow an administrator to modify their
    local copy of the WSDL to be more easily parsed
    by the driver.
    
    Change-Id: I911e0644dc062ac1ffb6afba294eacb8f54bf580

---
+++ tools/wsdl/vmware_cache_wsdl.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import collections
+import optparse
+import os
+import sys
+import urllib
+
+"""
+A tool to help you cache vSphere WSDL locally.
+
+Use with vSphere 5.0 and OpenStack nova compute driver Grizzly or later.
+
+If you are using OpenStack Nova compute driver with grizzly or havana along
+side vSphere API version 5.0 or earlier you may have a WSDL parse exception
+on driver initialization. This is due to the sensitivity of the Python XML
+parser. It will not tolerate missing XML or slightly malformed XML. Version
+5.1 and later of vCenter have addressed this issue and made their WSDL files
+Python friendly.
+
+Targets a supplied vSphere server (vCenter or naked ESXi) and downloads the
+WSDL off of it. Substitutes missing XML content with parser friendly stubs.
+"""
+
+ROOT_WSDL_FILE = 'vimService.wsdl'
+WSDL_FILES = frozenset([
+    'vimService.wsdl',
+    'vim.wsdl',
+    'core-types.xsd',
+    'query-messagetypes.xsd',
+    'query-types.xsd',
+    'reflect-messagetypes.xsd',
+    'reflect-types.xsd',
+    'vim-messagetypes.xsd'])
+
+XSD_WITH_404 = """<?xml version="1.0" encoding="UTF-8"?>
+<schema
+   targetNamespace="urn:reflect"
+   xmlns="http://www.w3.org/2001/XMLSchema"
+   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+   elementFormDefault="qualified">
+</schema>
+"""
+
+parser = optparse.OptionParser("usage: %%prog [options]")
+
+parser.add_option("-H", "--host",
+                  dest="hostname",
+                  default=None,
+                  type="string",
+                  help="specify hostname extract WSDL from")
+
+parser.add_option("-D", "--dest",
+                  dest="dest_directory",
+                  default="/opt/stack/wsdl",
+                  type="string",
+                  help="specify destination directory for WSDL files")
+
+(options, args) = parser.parse_args()
+
+if not( options.hostname and options.dest_directory ):
+    parser.print_help()
+    sys.exit()
+
+for wsdl_file in WSDL_FILES:
+    remote_file_path = "https://%s/sdk/%s" % (options.hostname, wsdl_file)
+    local_file_path = os.path.join(options.dest_directory, wsdl_file)
+    local_file = open(local_file_path, 'w')
+    remote_file = None
+
+    try:
+        remote_file = urllib.urlopen(remote_file_path)
+    except IOError as e:
+        print e.message
+        print "Could not open %s" % remote_file_path
+        os.sys.exit(1)
+
+    code = remote_file.getcode()
+    if code == 200:
+        print "%d: downloading %s" % (code, wsdl_file)
+        local_file.write(remote_file.read())
+    elif code == 404:
+        print "404: fixing missing %s" % wsdl_file
+        local_file.write(XSD_WITH_404)
+    else:
+        print "%s returned code %d" % (remote_file_path, code)
+
+    remote_file.close()
+    local_file.close()
+
+local_file = open(os.path.join( options.dest_directory, ROOT_WSDL_FILE ), 'r')
+local_file_name = os.path.abspath(local_file.name)
+local_file.close()
+print "# use the following location for your driver"
+print "VMWAREAPI_WSDL_LOC=file://%s" % local_file_name
