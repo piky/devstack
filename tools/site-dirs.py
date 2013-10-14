4af11ce2b0672abb6b5774fd31c07363fe13b414
Florian Haas <florian@hastexo.com>
2013-10-14 17:23:25 +0200

    fixup_stuff.sh: un-break site package detection for Python 2.6
    SITE_DIRS relied on the availability of site.getsitepackages() which
    is a Python 2.7 addition. Since Python 2.6 is the latest Python
    available on RHEL/CentOS 6, this would break DevStack on that
    platform.
    
    * Put the site packages detection magic into a separate .py script
    * Continue to use the site.getsitepackages() method but fall back to
      distutils.sysconfig if the former fails.
      Inspired by:
      https://docs.djangoproject.com/en/dev/topics/install/#s-installing-the-development-version-without-pip
    * Populate SITE_DIRS from an invocation of that script, rather than
      from inline Python.
    
    Fixes bug 1239742.
    
    Change-Id: Ifdf9af5afbc16214b9541b0a627cd933e63d4f82

---
+++ tools/site-dirs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import site
+import os
+
+# Try to figure out the site-packages directory using the Python 2.7+
+# getsitepackages() method. If that fails, fall back to the distutils
+# method that is also expected to work on RHEL 6.x (Python 2.6).
+try:
+    print(os.linesep.join(site.getsitepackages()))
+except AttributeError:
+    from distutils.sysconfig import get_python_lib
+    print(get_python_lib())
