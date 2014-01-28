a2a29dd1c0489a654667f219539ff3032acafac6
Kyle Mestery <kmestery@cisco.com>
2014-01-28 20:29:18 +0000

    Add support for the OpenDaylight Neutron MechanismDriver
    This commit adds a post script to handle configuration of some Open vSwitch values
    on compute hosts when using Neutron with the OpenDaylight MechanismDriver.
    
    Closes-bug: #1273917
    
    Change-Id: I696e7c7fe63c835f90c56105775def305a702877

---
+++ extras.d/80-opendaylight.sh
@@ -0,0 +1,41 @@
+# opendaylight.sh - DevStack extras script
+
+ODL_FOUND=$(echo $Q_ML2_PLUGIN_MECHANISM_DRIVERS | grep -q opendaylight ; echo $?)
+if [ "$ODL_FOUND" == "0" ] ; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # OpenDaylight configuration comes after all services are up
+        :
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Initializing OpenDaylight"
+        ODL_LOCAL_IP=${ODL_LOCAL_IP:-$HOST_IP}
+        if [ "$ODL_MGR_IP" == "" ]; then
+            ODL_MGR_IP=$(grep url /$Q_PLUGIN_CONF_FILE | sed -e s'/.*\/\///'g -e 's/\:.*//'g)
+        fi
+        ODL_MGR_PORT=${ODL_MGR_PORT:-6640}
+        read ovstbl <<< $(sudo ovs-vsctl get Open_vSwitch . _uuid)
+        sudo ovs-vsctl set-manager tcp:$ODL_MGR_IP:$ODL_MGR_PORT
+        sudo ovs-vsctl set Open_vSwitch $ovstbl other_config={"local_ip"="$ODL_LOCAL_IP"}
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        sudo ovs-vsctl del-manager
+        BRIDGES=$(sudo ovs-vsctl list-br)
+        for bridge in $BRIDGES ; do
+            sudo ovs-vsctl del-controller $bridge
+        done
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
