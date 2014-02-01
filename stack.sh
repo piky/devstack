f262b2127128b83ccf7ffaff08ea9ffc3a72903c
Sean Dague <sean.dague@samsung.com>
2014-02-01 15:43:04 -0500

    wip: how far can we get with errexit
    devstack fails can often happen much later than the real failure,
    see if setting errexit is viable.
    
    Change-Id: I843f19828c8c39f7913bbfc970a69a4a1464c5a0

--- stack.sh
+++ stack.sh
@@ -584,6 +584,10 @@ failed() {
     exit $r
 }
 
+# This script exits on an error so that errors don't compound and you see
+# only the first error that occurred.
+set -o errexit
+
 # Print the commands being run so that we can see the command that triggers
 # an error.  It is also useful for following along as the install occurs.
 set -o xtrace
