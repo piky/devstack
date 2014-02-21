b2a2a4938db8607b4a238833f45197d06a773dff
Ian Wienand <iwienand@redhat.com>
2014-02-21 15:35:08 +1100

    Enforce function declaration format in bash8
    Check that function calls look like ^function foo {$ in bash8, and fix
    all existing failures of that check.  Add a note to HACKING.rst
    
    Change-Id: Ic19eecb39e0b20273d1bcd551a42fe400d54e938

--- tools/bash8.py
+++ tools/bash8.py
@@ -34,6 +34,10 @@
 #
 # - E010: *do* not on the same line as *for*
 # - E011: *then* not on the same line as *if*
+#
+# Function related
+# - E020: check function signature is "function foo {"
+
 
 import argparse
 import fileinput
@@ -95,6 +99,21 @@ def check_indents(line):
         if (len(m.group('indent')) % 4) != 0:
             print_error('E003: Indent not multiple of 4', line)
 
+def check_function_decl(line):
+    failed = False
+    if line.startswith("function"):
+        if not re.search('^function [\w-]* \{$', line):
+            failed = True
+    else:
+        # catch the case without "function", e.g.
+        # things like '^foo() {'
+        if re.search('^\s*?\(\)\s*?\{', line):
+            failed = True
+
+    if failed:
+        print_error('E020: Function declaration not in format '
+                    ' "^function name {$"', line)
+
 
 def starts_multiline(line):
     m = re.search("[^<]<<\s*(?P<token>\w+)", line)
@@ -135,6 +154,7 @@ def check_files(files, verbose):
         check_indents(logical_line)
         check_for_do(logical_line)
         check_if_then(logical_line)
+        check_function_decl(logical_line)
 
 
 def get_options():
