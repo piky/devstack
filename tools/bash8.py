6c2fba0880c34722489dd8fe96da8622582e74b0
Ian Wienand <iwienand@redhat.com>
2014-02-21 16:14:29 +1100

    Fix heredoc matching for trailing quote
    The file lib/nova_plugins/hypervisor-libvirt does heredoc's like this:
    
    ---
    bash -c "cat <<EOF > foo
    ...
    EOF"
    ---
    
    The trailing quote was throwing the matching in bash8 off and it kept
    appending the next file as if it was still part of the heredoc.
    
    This fixes the match to recognize this, and additonally adds a check
    that raises an error if we finish a file and we still seem to be in a
    heredoc
    
    Change-Id: I5e547d87b3921fc7ce6588c28f074e5c9f489c1f

--- tools/bash8.py
+++ tools/bash8.py
@@ -106,7 +106,7 @@ def starts_multiline(line):
 
 def end_of_multiline(line, token):
     if token:
-        return re.search("^%s\s*$" % token, line) is not None
+        return re.search("^%s[\s\"]*$" % token, line) is not None
     return False
 
 
@@ -115,6 +115,11 @@ def check_files(files):
     logical_line = ""
     token = False
     for line in fileinput.input(files):
+
+        if fileinput.isfirstline():
+            if in_multiline:
+                raise RuntimeError("Last file ended but still in a heredoc?")
+
         # NOTE(sdague): multiline processing of heredocs is interesting
         if not in_multiline:
             logical_line = line
