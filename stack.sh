d841167cd7d31522af4062e5786c759f45ada602
Bartosz Górski <bartosz.gorski@ntti3.com>
2014-02-28 14:15:19 +0100

    Adds support for multi-region
    Change-Id: Ib85fe7cb375692b04aca4c46f61ba7e1fbfa501b
    Implements: blueprint multi-region

--- stack.sh
+++ stack.sh
@@ -333,6 +333,9 @@ SERVICE_TIMEOUT=${SERVICE_TIMEOUT:-60}
 SSL_BUNDLE_FILE="$DATA_DIR/ca-bundle.pem"
 rm -f $SSL_BUNDLE_FILE
 
+# Region
+OS_REGION_NAME=${OS_REGION_NAME:-RegionOne}
+
 
 # Configure Projects
 # ==================
@@ -721,8 +724,10 @@ git_clone $OPENSTACKCLIENT_REPO $OPENSTACKCLIENT_DIR $OPENSTACKCLIENT_BRANCH
 setup_develop $OPENSTACKCLIENT_DIR
 
 if is_service_enabled key; then
-    install_keystone
-    configure_keystone
+    if [ "$KEYSTONE_AUTH_HOST" == "$SERVICE_HOST" ]; then
+        install_keystone
+        configure_keystone
+    fi
 fi
 
 if is_service_enabled s-proxy; then
@@ -921,8 +926,11 @@ fi
 
 if is_service_enabled key; then
     echo_summary "Starting Keystone"
-    init_keystone
-    start_keystone
+
+    if [ "$KEYSTONE_AUTH_HOST" == "$SERVICE_HOST" ]; then
+        init_keystone
+        start_keystone
+    fi
 
     # Set up a temporary admin URI for Keystone
     SERVICE_ENDPOINT=$KEYSTONE_AUTH_URI/v2.0
@@ -963,6 +971,7 @@ if is_service_enabled key; then
     export OS_TENANT_NAME=admin
     export OS_USERNAME=admin
     export OS_PASSWORD=$ADMIN_PASSWORD
+    export OS_REGION_NAME=$OS_REGION_NAME
 fi
 
 
