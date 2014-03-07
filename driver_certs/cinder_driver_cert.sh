c2338d9292ae3e44cccd5aa522bae8cbb295c4cf
Ryan McNair <rdmcnair@us.ibm.com>
2014-03-07 05:31:56 +0000

    cinder_driver_certs.sh always passes driver
    In cinder_driver_certs.sh, the tests are run and output is
    piped to "tee".  Checking the exit code of this command
    only checks that "tee" exited properly.  Instead we should
    check the exit code for the first part of the pipe.
    
    Change-Id: I58311aa0e2c779467d39c4477c4ea39d14eda207
    Closes-Bug: #1289129

--- driver_certs/cinder_driver_cert.sh
+++ driver_certs/cinder_driver_cert.sh
@@ -93,7 +93,7 @@ sleep 5
 # run tempest api/volume/test_*
 log_message "Run the actual tempest volume tests (./tools/pretty_tox.sh api.volume)...", True
 ./tools/pretty_tox.sh api.volume 2>&1 | tee -a $TEMPFILE
-if [[ $? = 0 ]]; then
+if [[ "${PIPESTATUS[0]}" = 0 ]]; then
     log_message "CONGRATULATIONS!!!  Device driver PASSED!", True
     log_message "Submit output: ($TEMPFILE)"
     exit 0
