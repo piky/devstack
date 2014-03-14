5f568d53a9ab00137b5ef81d1a9e364183b2e709
Steve Martinelli <stevemar@ca.ibm.com>
2014-03-14 00:11:03 -0500

    Update keystone_data.sh to use openstackclient commands
    Updated files/keystone_data.sh to use openstackclient since
    keystoneclient is becoming deprecated.
    
    Change-Id: I5d252f53a33d2ec164ae4c78162b0de0f71f8266

--- files/keystone_data.sh
+++ files/keystone_data.sh
@@ -36,9 +36,9 @@ SERVICE_TENANT_NAME=${SERVICE_TENANT_NAME:-service}
 # The admin role in swift allows a user to act as an admin for their tenant,
 # but ResellerAdmin is needed for a user to act as any tenant. The name of this
 # role is also configurable in swift-proxy.conf
-keystone role-create --name=ResellerAdmin
+openstack role create ResellerAdmin
 # Service role, so service users do not have to be admins
-keystone role-create --name=service
+openstack role create service
 
 
 # Services
@@ -47,43 +47,43 @@ keystone role-create --name=service
 if [[ "$ENABLED_SERVICES" =~ "n-api" ]] && [[ "$ENABLED_SERVICES" =~ "s-proxy" || "$ENABLED_SERVICES" =~ "swift" ]]; then
     # Nova needs ResellerAdmin role to download images when accessing
     # swift through the s3 api.
-    keystone user-role-add \
-        --tenant $SERVICE_TENANT_NAME \
-        --user nova \
-        --role ResellerAdmin
+    openstack role add \
+        ResellerAdmin \
+        --project $SERVICE_TENANT_NAME \
+        --user nova
 fi
 
 # Glance
 if [[ "$ENABLED_SERVICES" =~ "g-api" ]]; then
-    keystone user-create \
-        --name=glance \
-        --pass="$SERVICE_PASSWORD" \
-        --tenant $SERVICE_TENANT_NAME \
-        --email=glance@example.com
-    keystone user-role-add \
-        --tenant $SERVICE_TENANT_NAME \
-        --user glance \
-        --role service
+    openstack user create \
+        glance \
+        --password "$SERVICE_PASSWORD" \
+        --project $SERVICE_TENANT_NAME \
+        --email glance@example.com
+    openstack role add \
+        service \
+        --project $SERVICE_TENANT_NAME \
+        --user glance
     # required for swift access
     if [[ "$ENABLED_SERVICES" =~ "s-proxy" ]]; then
-        keystone user-create \
-            --name=glance-swift \
-            --pass="$SERVICE_PASSWORD" \
-            --tenant $SERVICE_TENANT_NAME \
-            --email=glance-swift@example.com
-        keystone user-role-add \
-            --tenant $SERVICE_TENANT_NAME \
-            --user glance-swift \
-            --role ResellerAdmin
+        openstack user create \
+            glance-swift \
+            --password "$SERVICE_PASSWORD" \
+            --project $SERVICE_TENANT_NAME \
+            --email glance-swift@example.com
+        openstack role add \
+            ResellerAdmin \
+            --project $SERVICE_TENANT_NAME \
+            --user glance-swift
     fi
     if [[ "$KEYSTONE_CATALOG_BACKEND" = 'sql' ]]; then
-        keystone service-create \
-            --name=glance \
-            --type=image \
-            --description="Glance Image Service"
-        keystone endpoint-create \
+        openstack service create \
+            glance \
+            --type image \
+            --description "Glance Image Service"
+        openstack endpoint create \
+            glance \
             --region RegionOne \
-            --service glance \
             --publicurl "http://$SERVICE_HOST:9292" \
             --adminurl "http://$SERVICE_HOST:9292" \
             --internalurl "http://$SERVICE_HOST:9292"
@@ -93,21 +93,22 @@ fi
 # Ceilometer
 if [[ "$ENABLED_SERVICES" =~ "ceilometer" ]] && [[ "$ENABLED_SERVICES" =~ "s-proxy" || "$ENABLED_SERVICES" =~ "swift" ]]; then
     # Ceilometer needs ResellerAdmin role to access swift account stats.
-    keystone user-role-add --tenant $SERVICE_TENANT_NAME \
-        --user ceilometer \
-        --role ResellerAdmin
+    openstack role add \
+        ResellerAdmin \
+        --project $SERVICE_TENANT_NAME \
+        --user ceilometer
 fi
 
 # EC2
 if [[ "$ENABLED_SERVICES" =~ "n-api" ]]; then
     if [[ "$KEYSTONE_CATALOG_BACKEND" = 'sql' ]]; then
-        keystone service-create \
-            --name=ec2 \
-            --type=ec2 \
-            --description="EC2 Compatibility Layer"
-        keystone endpoint-create \
+        openstack service create \
+            ec2 \
+            --type ec2 \
+            --description "EC2 Compatibility Layer"
+        openstack endpoint create \
+            ec2 \
             --region RegionOne \
-            --service ec2 \
             --publicurl "http://$SERVICE_HOST:8773/services/Cloud" \
             --adminurl "http://$SERVICE_HOST:8773/services/Admin" \
             --internalurl "http://$SERVICE_HOST:8773/services/Cloud"
@@ -117,30 +118,32 @@ fi
 # S3
 if [[ "$ENABLED_SERVICES" =~ "n-obj" || "$ENABLED_SERVICES" =~ "swift3" ]]; then
     if [[ "$KEYSTONE_CATALOG_BACKEND" = 'sql' ]]; then
-        keystone service-create \
-            --name=s3 \
-            --type=s3 \
-            --description="S3"
-        keystone endpoint-create \
+        openstack service create \
+            s3 \
+            --type s3 \
+            --description "S3"
+        openstack endpoint create \
+            s3 \
             --region RegionOne \
-            --service s3 \
             --publicurl "http://$SERVICE_HOST:$S3_SERVICE_PORT" \
             --adminurl "http://$SERVICE_HOST:$S3_SERVICE_PORT" \
             --internalurl "http://$SERVICE_HOST:$S3_SERVICE_PORT"
     fi
 fi
 
+# Tempest
 if [[ "$ENABLED_SERVICES" =~ "tempest" ]]; then
     # Tempest has some tests that validate various authorization checks
     # between two regular users in separate tenants
-    keystone tenant-create \
-        --name=alt_demo
-    keystone user-create \
-        --name=alt_demo \
-        --pass="$ADMIN_PASSWORD" \
-        --email=alt_demo@example.com
-    keystone user-role-add \
-        --tenant alt_demo \
-        --user alt_demo \
-        --role Member
+    openstack project create \
+        alt_demo
+    openstack user create \
+        alt_demo \
+        --password "$ADMIN_PASSWORD" \
+        --email alt_demo@example.com \
+        --project alt_demo
+    openstack role add \
+        Member \
+        --project alt_demo \
+        --user alt_demo
 fi
