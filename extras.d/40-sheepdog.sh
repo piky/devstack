b53454ea1f2372c0293672e4c3831771f13b4f62
Scott Devoid <devoid@anl.gov>
2014-04-17 20:26:07 +0000

    extra.d to use Sheepdog as devstack backing store.
    Configures Glance and Cinder to use a single Sheepdog
    cluster--3 processes running on the single machine.
    
    Change-Id: I05563b049c33fce3939eb90b6deaee41a78a3532

---
+++ extras.d/40-sheepdog.sh
@@ -0,0 +1,27 @@
+# sheepdog.sh - Devstack extras script to enable sheepdog storage backend
+
+if is_service_enabled sheepdog; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source
+        source $TOP_DIR/lib/sheepdog
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Sheepdog"
+        install_sheepdog
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # NOTE (scott-devoid): we do everything here because we need to have
+        # sheepdog started before other openstack components, e.g. glance in
+        # order to upload iamges to the sheepdog cluster.
+        echo_summary "Configuring Sheepdog cluster"
+        configure_sheepdog
+        echo_summary "Starting Sheepdog cluster"
+        start_sheepdog
+    fi
+    if [[ "$1" == "unstack" ]]; then
+        echo_summary "Stopping Sheepdog cluster"
+        stop_sheepdog
+    fi
+    if [[ "$1" == "clean" ]]; then
+        echo_summary "Clearing Sheepdog cluster of data"
+        cleanup_sheepdog
+    fi
+fi
