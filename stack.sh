2f1f06007895e8028e092a346a058efffb657596
Attila Fazekas <afazekas@redhat.com>
2014-05-28 09:52:22 +0200

    Faster nova fixed key generation
    Using bc 64 times in loop is too verbose and slow,
    replacing the echo/bc loop with hexdump and urandom.
    
    Change-Id: If6a86dfaf0c21e2635c6de0a7b96a8ed7ec5b507

--- stack.sh
+++ stack.sh
@@ -1164,11 +1164,7 @@ fi
 
 # Create a randomized default value for the keymgr's fixed_key
 if is_service_enabled nova; then
-    FIXED_KEY=""
-    for i in $(seq 1 64); do
-        FIXED_KEY+=$(echo "obase=16; $(($RANDOM % 16))" | bc);
-    done;
-    iniset $NOVA_CONF keymgr fixed_key "$FIXED_KEY"
+    iniset $NOVA_CONF keymgr fixed_key `hexdump -n 32 -v -e '/1 "%02x"' /dev/urandom`
 fi
 
 if is_service_enabled zeromq; then
