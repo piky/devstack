8be469810b5a0f5a01a7aaaefb1152964b25ec0a
Brian Haley <brian.haley@hp.com>
2014-06-27 11:45:50 -0400

    Patch to allow merging of upstream reviews into devstack
    The main goal is to facilitate easier testing of the Neutron DVR
    code while it is still in development and not in the neutron
    repository.
    
    For that reason this patch should *NOT* be merged, but comments
    are welcome since functionality like this might be good to have.
    
    Things you should do before running stack.sh:
    
    1) Copy your ssh key to ~stack/.ssh
    2) Start the ssh authentication agent and add your key.  This will
       prevent you from being prompted for your password during cloning
       and merging:
    
    	$ ssh-agent
    	$ ssh-add
    
    3) Add your gerrit id to local.conf:
    
    	GERRIT_USER=john-doe
    
    4) Add all upstream review id's to local.conf:
    
    	NEUTRON_UPSTREAM_PATCHES="..."
    
       This has been pre-populated in samples/local.conf, but other
       review id's might be necessary as time goes on.
    
    Possible ways to deal with merge failures:
    
    1) Try running the merge script by hand, it's been provided separately:
    
    	$ cd /opt/stack/neutron
    	$ ~/devstack/merge-dvr-patchsets.sh $(review ids)
    
    2) Provide comments on the upstream reviews so that they get
       merge conflicts fixed and/or rebased appropriately.
    
    Future work on this patchset can enable configuration settings
    in neutron config files to turn-on the DVR functionality.  That
    work is TBD.
    
    Change-Id: I669e054a8489fe7ac7f5c00df6535349d477e8c4

---
+++ merge-dvr-patchsets.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+gerrit_remote=gerrit
+gerrit_user=john-doe
+gerrit_repo=openstack/neutron
+gerrit_ssh=review.openstack.org:29418
+gerrit_url="ssh://${gerrit_user}@${gerrit_ssh}/${gerrit_repo}.git"
+
+git remote add $gerrit_remote $gerrit_url 2>/dev/null || true
+
+# set user, host, port, and project from git config
+eval $(echo "$(git config remote.$gerrit_remote.url)" |
+    sed 's,ssh://\(.*\)@\(.*\):\([[:digit:]]*\)/\(.*\).git,user=\1 host=\2 port=\3 project=\4,')
+
+gerrit() {
+    ssh $user@$host -p $port gerrit ${1+"$@"}
+}
+
+get_patchset_information() {
+    eval $(gerrit query --current-patch-set --dependencies $1 |
+        awk '
+            BEGIN {
+                mode="main"
+                printf "dependsOn=()\n"
+            }
+            / currentPatchSet:/ { mode="currentPatchSet" }
+            / dependsOn:/ { mode="dependsOn" }
+            / neededBy:/ { mode="neededBy" }
+            / ref:/ {
+                if (mode=="currentPatchSet") {
+                    printf "new_patch_ref=%s\n", $2
+                }
+            }
+            / open:/ { printf "open=%s\n", $2 }
+            / revision:/ {
+                if (mode=="currentPatchSet") {
+                    printf "revision=%s\n", $2
+                }
+            }
+            / number:/ {
+                if (mode=="main") {
+                    printf "review_num=%s\n", $2
+                }
+                if (mode=="currentPatchSet") {
+                    printf "new_patchset=%s\n", $2
+                }
+                if (mode=="dependsOn") {
+                    printf "dependsOn+=(%s)\n", $2
+                }
+            }
+        ')
+}
+
+edges=()
+for arg in "$@"
+do
+    get_patchset_information $arg
+
+    if [ "$open" != "true" ]
+    then
+        echo >&2 "Skipping $arg because it is closed"
+        continue
+    fi
+
+    git fetch $gerrit_remote $new_patch_ref
+
+    # Organize dependencies
+    for dependency in "${dependsOn[@]}"
+    do
+        get_patchset_information $dependency
+        [ "$open" = "true" ] && edges+=($arg $dependency)
+    done
+done
+
+# Topologically sort the patches
+sorted=($(for edge in "${edges[@]}"
+do
+    echo $edge
+done | tsort | tac))
+
+revisions=()
+
+# Check out the base patch
+get_patchset_information ${sorted[0]}
+revisions+=($revision)
+unset sorted[0]
+git checkout $revision
+
+echo "${sorted[@]}"
+
+for ref in "${sorted[@]}"
+do
+    get_patchset_information $ref
+    revisions+=($revision)
+    git cherry-pick --no-commit $revision
+    if [ $? -ne 0 ]
+    then
+        echo "Fix er up"
+        bash
+    fi
+done
+
+tree=$(git write-tree)
+commit=$(git commit-tree -m "Merge DVR patches together" \
+         $(for rev in ${revisions[@]}; do echo -p $rev; done) \
+         $tree)
+git checkout $commit
