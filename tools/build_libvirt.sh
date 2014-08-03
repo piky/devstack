ad5dc46bd733801433e403834bb8eaf6b7b34277
Michele Paolino <m.paolino@virtualopensystems.com>
2014-08-03 19:14:42 +0200

    Support for libvirt/QEMU tar releases
    Enable to install libvirt and QEMU from tar releases.
    The definition of the variables LIBVIRT_VERSION and QEMU_VERSION
    will result in the compilation and the installation of libvirt and
    QEMU from the tar releases pointed in the variables.
    
    local.conf example:
    QEMU_VERSION=2.1.2
    LIBVIRT_VERSION=1.2.8
    
    Change-Id: I89d20609fbfc8791892ab103c7b25c1334e3e94d
    Signed-off-by: Michele Paolino <m.paolino@virtualopensystems.com>

---
+++ tools/build_libvirt.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# build_libvirt.sh
+#
+# Install libvirt and its dependencies.
+#
+
+# Echo commands
+set -o xtrace
+
+# Exit on error to stop unexpected errors
+set -o errexit
+
+function usage {
+    echo "$0 - Install libvirt from tar releases."
+    echo ""
+    echo "Usage: $0 <LIBVIRT_VERSION>"
+    echo ""
+    echo "Example: $0 1.2.7"
+}
+
+# Keep track of the current directory
+TOOLS_DIR=$(cd $(dirname "$0") && pwd)
+TOP_DIR=$(cd $TOOLS_DIR/..; pwd)
+
+# Import common functions and variables
+source $TOP_DIR/functions
+source $TOP_DIR/stackrc
+
+# Find the cache dir
+FILES=$TOP_DIR/files
+
+if [[ -z "$1" ]]; then
+    usage
+    exit 1
+fi
+
+LIBVIRT_VERSION=$1
+
+RECLONE=$(trueorfalse False $RECLONE)
+
+echo "Installing libvirt $LIBVIRT_VERSION"
+
+# If this value is not user defined, use the official link
+LIBVIRT_URL_BASE=${LIBVIRT_URL_BASE:-https://libvirt.org/sources/}
+
+# libvirt is released as .tar.gz
+LIBVIRT_FILE=libvirt-"$LIBVIRT_VERSION".tar.gz
+LIBVIRT_DIR="$DEST"/libvirt-"$LIBVIRT_VERSION"
+
+LIBVIRT_URL="$LIBVIRT_URL_BASE""$LIBVIRT_FILE"
+
+# libvirtd debug variables
+NOW=`date +"%F-%T"`
+WHERE=`hostname`
+EPOCH=`date +%s`
+
+echo "Installing libvirt build dependencies"
+if is_ubuntu; then
+    sudo apt-get build-dep libvirt -y
+    install_package python-guestfs
+        if [[ ${DISTRO} =~ (precise) ]]; then
+            sudo apt-get install libnl-route-3-dev -y
+        fi
+elif is_fedora || is_suse; then
+    install_package yum-utils
+    sudo yum-builddep libvirt -y
+    install_package python-libguestfs
+fi
+
+echo "Downloading the libvirt sources"
+wget -N "$LIBVIRT_URL" -P "$FILES"
+
+if [[ ! -d "$LIBVIRT_DIR" || "$RECLONE" = "True" ]]; then
+    echo "Configuring libvirt"
+    tar -xf "$FILES"/"$LIBVIRT_FILE" -C "$DEST"
+    cd "$LIBVIRT_DIR"
+    ./configure --prefix=/ --exec-prefix=/usr \
+        --with-packager="OpenStack DevStack $WHERE $NOW" \
+        --with-packager_version="$EPOCH"
+fi
+
+echo "Compiling libvirt"
+cd "$LIBVIRT_DIR"
+make -j"$(nproc)"
+sudo make install
