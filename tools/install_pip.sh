4420ebcd0aa28f7a3b1ba3be94c78e1a4bf909fe
Monty Taylor <mordred@inaugust.com>
2014-09-05 14:31:38 -0700

    WIP - DO NOT MERGE - Test pre-release setuptools
    Change-Id: I7bb83ac641c3c6f7ec55b8a0787aea0e00105398

--- tools/install_pip.sh
+++ tools/install_pip.sh
@@ -83,6 +83,8 @@ if [[ -n $PYPI_ALTERNATIVE_URL ]]; then
     configure_pypi_alternative_url
 fi
 
-pip_install -U setuptools
+pip_install https://bitbucket.org/pypa/setuptools/downloads/setuptools-8.0b1.zip
+
+pip_install -U git+git://github.com/dstufft/pip@a83037f29c
 
 get_versions
