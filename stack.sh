738b9b5acbe5b9a7217bccb89d867f687c92aef3
Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
2014-09-15 15:35:15 +0000

    Gnocchi support
    This change adds gnocchi support to devstack
    
    Change-Id: I771935afd3511547209ab0dab215725348b6cb27

--- stack.sh
+++ stack.sh
@@ -559,6 +559,7 @@ source $TOP_DIR/lib/nova
 source $TOP_DIR/lib/cinder
 source $TOP_DIR/lib/swift
 source $TOP_DIR/lib/ceilometer
+source $TOP_DIR/lib/gnocchi
 source $TOP_DIR/lib/heat
 source $TOP_DIR/lib/neutron
 source $TOP_DIR/lib/baremetal
@@ -843,6 +844,12 @@ if is_service_enabled ceilometer; then
     configure_ceilometer
 fi
 
+if is_service_enabled gnocchi; then
+    install_gnocchi
+    echo_summary "Configuring Gnocchi"
+    configure_gnocchi
+fi
+
 if is_service_enabled heat; then
     install_heat
     install_heat_other
@@ -1010,6 +1017,9 @@ if is_service_enabled key; then
 
     if is_service_enabled ceilometer; then
         create_ceilometer_accounts
+    fi
+    if is_service_enabled gnocchi; then
+        create_gnocchi_accounts
     fi
 
     if is_service_enabled swift; then
@@ -1288,6 +1298,12 @@ if is_service_enabled ceilometer; then
     init_ceilometer
     start_ceilometer
 fi
+if is_service_enabled gnocchi; then
+    echo_summary "Starting Gnocchi"
+    init_gnocchi
+    start_gnocchi
+fi
+
 
 # Configure and launch heat engine, api and metadata
 if is_service_enabled heat; then
