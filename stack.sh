f53c2651e1b0f15eab7893e749fa27b1806581d7
Daniel Genin <Daniel.Genin@jhuapl.edu>
2014-10-31 15:01:29 -0400

    Adds support for LVM ephemeral storage in Nova
    DevStack currently lacks support for LVM ephemeral storage in Nova.
    This support is important for testing of Nova's LVM backend. The
    proposed change adds a default volume group, to be shared by Cinder
    and Nova. It also adds a configuration option NOVA_BACKEND, which
    must be LVM if it is set, that determines whether Nova should be
    configured to use LVM ephemeral storage.
    
    Change-Id: I4eb9afff3536fbcd563939f2d325efbb845081bb

--- stack.sh
+++ stack.sh
@@ -143,7 +143,7 @@ source $TOP_DIR/stackrc
 
 # Warn users who aren't on an explicitly supported distro, but allow them to
 # override check and attempt installation with ``FORCE=yes ./stack``
-if [[ ! ${DISTRO} =~ (precise|trusty|7.0|wheezy|sid|testing|jessie|f19|f20|rhel6|rhel7) ]]; then
+if [[ ! ${DISTRO} =~ (precise|raring|trusty|7.0|wheezy|sid|testing|jessie|f19|f20|rhel6|rhel7) ]]; then
     echo "WARNING: this script has not been tested on $DISTRO"
     if [[ "$FORCE" != "yes" ]]; then
         die $LINENO "If you wish to run this script anyway run with FORCE=yes"
@@ -572,6 +572,7 @@ source $TOP_DIR/lib/neutron
 source $TOP_DIR/lib/baremetal
 source $TOP_DIR/lib/ldap
 source $TOP_DIR/lib/dstat
+source $TOP_DIR/lib/lvm
 
 # Extras Source
 # --------------
@@ -979,6 +980,13 @@ init_service_check
 
 # A better kind of sysstat, with the top process per time slice
 start_dstat
+
+# Initialize default LVM volume group
+#------------------------------------
+
+if type init_default_volume_group > /dev/null 2>&1; then
+    init_default_volume_group
+fi
 
 # Start Services
 # ==============
