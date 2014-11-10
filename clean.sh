b701903604bc9856c4e1aed411efdb227afa4c11
lokesh <lokesh.s@hp.com>
2014-11-10 15:07:59 +0530

    fixes clean.sh script to delete ironic directory from /etc Closes-Bug: 1391083 Change-Id: Ic47161ab375716cc39ee1a7dd57034782717e323

--- clean.sh
+++ clean.sh
@@ -101,7 +101,7 @@ if is_service_enabled nova && [[ -r $NOVA_PLUGINS/hypervisor-$VIRT_DRIVER ]]; th
 fi
 
 # Clean out /etc
-sudo rm -rf /etc/keystone /etc/glance /etc/nova /etc/cinder /etc/swift /etc/heat /etc/neutron
+sudo rm -rf /etc/keystone /etc/glance /etc/nova /etc/cinder /etc/swift /etc/heat /etc/neutron /etc/ironic 
 
 # Clean out tgt
 sudo rm -f /etc/tgt/conf.d/*
