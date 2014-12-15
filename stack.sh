16bf9751b34920eaf358462a0eb46da1d17271d1
Zhang Jinnan <zhang.jinnan@99cloud.net>
2014-12-14 19:19:53 -0500

    RHEL7 does not include Open vSwitch in the default repos, but it is available via the RDO repo.  This patch automatically configures the RDO repo for RHEL6.
    Fixed the bug 1402390.
    
    Change-Id: Ibcf1740a7d5d7a52ef7c949ad9610db42f4406b6

--- stack.sh
+++ stack.sh
@@ -221,6 +221,15 @@ if is_fedora && [ $DISTRO == "rhel6" ]; then
         yum_install $RHEL6_RDO_REPO_RPM || \
             die $LINENO "Error installing RDO repo, cannot continue"
     fi
+elif is_fedora && [ $DISTRO == "rhel7"]; then
+    # Installing Open vSwitch on RHEL requires enabling the RDO repo.
+    RHEL7_RDO_REPO_RPM=${RHEL7_RDO_REPO_RPM:-"https://repos.fedorapeople.org/repos/openstack/openstack-juno/rdo-release-juno-1.noarch.rpm"}
+    RHEL7_RDO_REPO_ID=${RHEL7_RDO_REPO_ID:-"openstack-juno"}
+    if ! sudo yum repolist enabled $RHEL7_RDO_REPO_ID | grep -q $RHEL7_RDO_REPO_ID; then
+        echo "RDO repo not detected; installing"
+        yum_install $RHEL7_RDO_REPO_RPM || \
+            die $LINENO "Error installing RDO repo, cannot continue"
+    fi 
 fi
 
 if is_fedora && [[ $DISTRO == "rhel6" || $DISTRO == "rhel7" ]]; then
