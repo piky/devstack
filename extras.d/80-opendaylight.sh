063d28f1c3932709722b7a71e9140335deb3fd12
Swaminathan Vasudevan <swaminathan.vasudevan@hp.com>
2015-02-01 19:41:02 -0800

    Add support in devstack to pull Stackforge/Networking-ODL
    This patch adds support to pull the repo from stackforge/
    Networking-odl and install the dependent files before
    running the OpenDaylight Controller.
    
    Change-Id: Ic2854ee576dced392399428bc2584cde2c6b8b44
    Closes-Bug: #1416947

--- extras.d/80-opendaylight.sh
+++ extras.d/80-opendaylight.sh
@@ -1,15 +1,16 @@
-# opendaylight.sh - DevStack extras script
+#opendaylight.sh - DevStack extras script
 
-if is_service_enabled odl-server odl-compute; then
+if is_service_enabled odl-neutron-integrated-server odl-neutron-only-server odl-compute; then
     # Initial source
     [[ "$1" == "source" ]] && source $TOP_DIR/lib/opendaylight
 fi
 
-if is_service_enabled odl-server; then
+if is_service_enabled odl-neutron-integrated-server; then
     if [[ "$1" == "source" ]]; then
         # no-op
         :
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_opendaylight_neutron_thin_ml2_driver
         install_opendaylight
         configure_opendaylight
         init_opendaylight
@@ -25,6 +26,30 @@ if is_service_enabled odl-server; then
     if [[ "$1" == "unstack" ]]; then
         stop_opendaylight
         cleanup_opendaylight
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
+
+if is_service_enabled odl-neutron-only-server; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_opendaylight_neutron_thin_ml2_driver
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        configure_ml2_odl
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
     fi
 
     if [[ "$1" == "clean" ]]; then
