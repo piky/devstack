5aaf7b525eaeb68361028cd2b6edf813e1926b10
Clark Boylan <clark.boylan@gmail.com>
2015-03-27 09:19:57 -0700

    Update libvirt cpu map before starting nova
    We are trying to get a working 64bit qemu cpu model in the gate for nova
    live migration testing. It appears that we need to make this change
    prior to nova starting. Use the install run phase to manage this.
    
    Change-Id: I00667713bfba67ab8cedbcb1660ff94d4f4bcc07

---
+++ extras.d/99-cpu_map.sh
@@ -0,0 +1,24 @@
+# cpu_map.sh - Extras script to install custom cpu_map
+if is_service_enabled n-cpu; then
+    if [[ "$1" == "install" ]]; then
+        local x86=0
+        local models=0
+        while read line ; do
+            echo $line
+            if [[ "$line" =~ "<arch name='x86'>" ]] && [[ "$x86" -eq 0 ]] ; then
+                x86=1
+            elif [[ "$line" =~ "</model>" ]] && [[ "$model" -eq 0 ]] ; then
+                models=1
+            fi
+            if [[ "$x86" -eq 1 ]] && [[ "$models" -eq 1 ]] ; then
+                echo "<model name='gate64'>"
+                echo "<model name='pentiumpro'/>"
+                echo "<feature name='lm'/>"
+                echo "</model>"
+                x86="done"
+                models="done"
+            fi
+        done < /usr/share/libvirt/cpu_map.xml
+        sudo service restart libvirt-bin
+    fi
+fi
