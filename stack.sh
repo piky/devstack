62e332c99fe135060ba61035f269832c13ddacfb
Peter Stachowski <peter@tesora.com>
2015-04-08 19:48:09 +0000

    Use openstack CLI instead of keystone
    When running './stack.sh' messages are output stating that
    the keystone CLI has been deprecated. These calls should be
    replaced to ones utilizing the openstack CLI program instead.
    
    Documentation examples were also updated to reflect the new syntax.
    
    Change-Id: Ib20b8940e317d150e5f6febb618e20bd85d13f8b
    Closes-Bug: #1441340

--- stack.sh
+++ stack.sh
@@ -1173,7 +1173,7 @@ fi
 # See https://help.ubuntu.com/community/CloudInit for more on ``cloud-init``
 
 if is_service_enabled g-reg; then
-    TOKEN=$(keystone token-get | grep ' id ' | get_field 2)
+    TOKEN=$(openstack token issue | grep ' id ' | get_field 2)
     die_if_not_set $LINENO TOKEN "Keystone fail to get token"
 
     echo_summary "Uploading images"
