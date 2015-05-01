e956db6d7a43429d2350db333b9378e035eb8ab9
Davanum Srinivas <davanum@gmail.com>
2015-04-30 21:10:48 -0400

    [WIP][EXPERIMENT] can nova-api be run under apache?
    Change-Id: I9fc0c601db2776d3e9084be84065e728e3f5d414

---
+++ files/nova-api.py
@@ -0,0 +1,25 @@
+from oslo_config import cfg
+from oslo_log import log as logging
+from paste import deploy
+
+from nova import config
+from nova import objects
+from nova import service  # noqa
+from nova import utils
+
+CONF = cfg.CONF
+
+config_files = ['/etc/nova/api-paste.ini', '/etc/nova/nova.conf']
+config.parse_args([], default_config_files=config_files)
+
+LOG = logging.getLogger(__name__)
+logging.setup(CONF, "nova")
+utils.monkey_patch()
+objects.register_all()
+
+conf = config_files[0]
+name = "osapi_compute"
+
+options = deploy.appconfig('config:%s' % conf, name=name)
+
+application = deploy.loadapp('config:%s' % conf, name=name)
