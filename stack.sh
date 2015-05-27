df8f63c598713d66a2a111a678e015a6150897b4
Kashyap Chamarthy <kchamart@redhat.com>
2015-05-27 21:59:32 +0200

    Remove Fedora 20 as supported distribution
    Fedora 20 will reach end of life on 23-JUN-2015[1]; remove it as
    supported distribution. Add Fedora 22 where applicable.
    
        - stack.sh: Remove Fedora 20 from list of supported distributions.
        - files/rpms/general: Remove 'f20' from NOPRIME.
        - lib/ceph: Remove 'f20' from the check_os_support_ceph()
          function.
        - doc/source/index.rst: s/Fedora 20/Fedora 21/
    
    [1] https://lists.fedoraproject.org/pipermail/devel-announce/2015-May/001586.html
    
    Change-Id: I8f2e1ddc24c071754b1cceb5bed5bdafdc9d9f79

--- stack.sh
+++ stack.sh
@@ -173,7 +173,11 @@ source $TOP_DIR/stackrc
 
 # Warn users who aren't on an explicitly supported distro, but allow them to
 # override check and attempt installation with ``FORCE=yes ./stack``
+<<<<<<< HEAD
 if [[ ! ${DISTRO} =~ (precise|trusty|7.0|wheezy|sid|testing|jessie|f20|f21|f22|rhel7) ]]; then
+=======
+if [[ ! ${DISTRO} =~ (precise|trusty|7.0|wheezy|sid|testing|jessie|f21|f22|rhel7) ]]; then
+>>>>>>> b8a5a5b... Remove Fedora 20 as supported distribution
     echo "WARNING: this script has not been tested on $DISTRO"
     if [[ "$FORCE" != "yes" ]]; then
         die $LINENO "If you wish to run this script anyway run with FORCE=yes"
