259e3902a52ae2c2f62ad93076cae298583f0d05
gong yong sheng <gong.yongsheng@99cloud.net>
2015-06-23 13:48:48 +0800

    Use non-in-place sed to change hosts
    Change-Id: I9f4e2c5988e05efbd3afbea99405981fcad68b0c

--- stack.sh
+++ stack.sh
@@ -312,9 +312,15 @@ safe_chown -R $STACK_USER $DATA_DIR
 # Configure proper hostname
 # Certain services such as rabbitmq require that the local hostname resolves
 # correctly.  Make sure it exists in /etc/hosts so that is always true.
+# Not to use 'sed -i', which does not run under docker container env, where
+# the /etc/hosts are mounted from host file
 LOCAL_HOSTNAME=`hostname -s`
 if [ -z "`grep ^127.0.0.1 /etc/hosts | grep $LOCAL_HOSTNAME`" ]; then
-    sudo sed -i "s/\(^127.0.0.1.*\)/\1 $LOCAL_HOSTNAME/" /etc/hosts
+    cat /etc/hosts | sed -e "s/\(^127.0.0.1.*\)/\1 $LOCAL_HOSTNAME/" | sudo tee /etc/hosts > /dev/null
+fi
+
+if [ -z "`grep ^::1 /etc/hosts | grep $LOCAL_HOSTNAME`" ]; then
+    cat /etc/hosts | sed -e "s/\(^::1.*\)/\1 $LOCAL_HOSTNAME/" | sudo tee /etc/hosts > /dev/null
 fi
 
 
