5297852a9d34334493131a9c397218f8ac57f44e
fumihiko kakuma <kakuma@valinux.co.jp>
2015-10-13 13:12:56 +0900

    Specify the default v3.25 to RYU_BRANCH in case of the ryu plugin
    Currently RYU_BRANCH variable is definded in stackrc by default master
    and it is used by both ofagent and ryu plugin.
    But in the latest version(3.26 and later) of ryu library, some rest
    api modules to be used by ryu plugin were removed.
    So we use refs/tags/v3.25 as default branch in the case of ryu plugin.
    
    Change-Id: Ie244f610260560d8efb2fb54ef3ecbb7e11968b7
    Closes-Bug: #1505561

--- tests/test_refs.sh
+++ tests/test_refs.sh
@@ -15,7 +15,7 @@
 
 echo "Ensuring we don't have crazy refs"
 
-REFS=`grep BRANCH stackrc | grep -v -- '-master' | grep -v -- '-stable/'`
+REFS=`grep BRANCH stackrc | grep -v -- '-master' | grep -v -- '-stable/' | grep -v -- '-refs/'`
 rc=$?
 if [[ $rc -eq 0 ]]; then
     echo "Branch defaults must be master. Found:"
