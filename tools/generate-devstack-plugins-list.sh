ba964b288f823bc060a4a52d5c54878ef2022f1e
Clint Adams <clint@gcfm.net>
2016-01-19 18:17:49 -0500

    Add script to autogenerate doc/source/plugin-registry.rst
    This generates the plugin-registry document from a static header,
    a scan of openstack/ git repositories, and a static footer.  It
    is intended to be run by a periodic job proposal bot to keep the
    list of plugins current.
    
    Change-Id: Ia04ab72900c8efd5d5289fbd7632201dcaa3e5d9

---
+++ tools/generate-devstack-plugins-list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+(
+declare -A plugins
+
+test -r data/devstack-plugins-registry.header && cat data/devstack-plugins-registry.header
+
+pushd ${git_dir:-/opt/openstack} >/dev/null
+for i in *; do
+    pushd ${i} >/dev/null
+    if output="$(git log --diff-filter=A --format='%cd' --date=short -1 -- devstack/plugin.sh)"; then
+        test -n "$output" && plugins[$i]=${output}
+    fi
+    popd >/dev/null
+done
+popd >/dev/null
+
+sorted_plugins=( $(for k in "${!plugins[@]}"; do echo "$k"; done | sort))
+
+for k in "${sorted_plugins[@]}"; do
+    project=${k:0:18}
+    giturl="git://git.openstack.org/openstack/${k:0:26}"
+    pdate="${plugins[$k]}"
+    printf "|%-18s|%-60s|%-12s|\n" "${project}" "${giturl}" "${pdate}"
+    printf "+------------------+------------------------------------------------------------+------------+\n"
+done
+
+test -r data/devstack-plugins-registry.footer && cat data/devstack-plugins-registry.footer
+) > doc/source/plugin-registry.rst
