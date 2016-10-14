0f050f7d5babd1526ae6b6746ec08797bac23ff3
Matt Riedemann <mriedem@us.ibm.com>
2016-10-14 19:48:43 -0400

    Stop using nova-manage network create
    nova-manage network create is deprecated, so this replaces
    it with the equivalent nova CLI from python-novaclient.
    
    Because the nova CLI hits nova-api and nova-api calls
    nova-net to create the network, which calls conductor
    to use the database, we have to start nova-conductor and
    nova-net before we can create the network now.
    
    Change-Id: I5c4291509841325e6123520131f30b64c847a17f

--- stack.sh
+++ stack.sh
@@ -1241,6 +1241,12 @@ if is_service_enabled n-api; then
     start_nova_api
 fi
 
+# Launch nova-conductor
+if is_service_enabled n-cond; then
+    echo_summary "Starting nova-conductor"
+    start_nova_process n-cond nova-conductor
+fi
+
 if is_service_enabled neutron-api; then
     echo_summary "Starting Neutron"
     start_neutron_api
@@ -1253,8 +1259,11 @@ elif is_service_enabled $DATABASE_BACKENDS && is_service_enabled n-net; then
         NM_CONF=${NOVA_CELLS_CONF}
     fi
 
+    echo_summary "Starting nova-network"
+    start_nova_process n-net nova-network
+
     # Create a small network
-    $NOVA_BIN_DIR/nova-manage --config-file $NM_CONF network create "$PRIVATE_NETWORK_NAME" $FIXED_RANGE 1 $FIXED_NETWORK_SIZE $NETWORK_CREATE_ARGS
+    nova network-create --fixed-range-v4 $FIXED_RANGE "$PRIVATE_NETWORK_NAME"
 
     # Create some floating ips
     $NOVA_BIN_DIR/nova-manage --config-file $NM_CONF floating create $FLOATING_RANGE --pool=$PUBLIC_NETWORK_NAME
