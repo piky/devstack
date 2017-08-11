29fb24347adf034c74e019e79a91c22a27425158
Sean Dague <sean@dague.net>
2017-08-11 15:37:39 -0400

    update branches for stable/pike
    Change-Id: I8c8b4ce1926af1fc5eb3844114b4c055ab38359c

--- tests/test_libs_from_pypi.sh
+++ tests/test_libs_from_pypi.sh
@@ -94,17 +94,6 @@ function test_libs_exist {
     echo "test_libs_exist PASSED"
 }
 
-function test_branch_master {
-    for lib in $ALL_LIBS; do
-        if [[ ${GITBRANCH[$lib]} != "master" ]]; then
-            echo "GITBRANCH for $lib not master (${GITBRANCH[$lib]})"
-            exit 1
-        fi
-    done
-
-    echo "test_branch_master PASSED"
-}
-
 set -o errexit
 
 test_libs_exist
