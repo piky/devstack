aaffca25b085df24b053f726e94e2c50a4de0d33
James E. Blair <jeblair@redhat.com>
2017-11-21 17:05:43 -0800

    Zuul: support plugin dependencies
    Change-Id: I81302e8988fe6498fea9f08ed66f5d0cc1fce161

---
+++ roles/write-devstack-local-conf/library/test.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from devstack_local_conf import LocalConf
+from collections import OrderedDict
+
+def main():
+    localrc = {'test_localrc': '1'}
+    local_conf = {'install':
+                  {'nova.conf':
+                   {'main':
+                    {'test_conf': '2'}}}}
+    services = {'cinder': True}
+    # We use ordereddict here to make sure the plugins are in the
+    # *wrong* order for testing.
+    plugins = OrderedDict([
+        ('bar', 'git://git.openstack.org/openstack/bar-plugin'),
+        ('foo', 'git://git.openstack.org/openstack/foo-plugin'),
+        ('baz', 'git://git.openstack.org/openstack/baz-plugin'),
+        ])
+    p = dict(localrc=localrc,
+             local_conf=local_conf,
+             services=services,
+             plugins=plugins,
+             base_dir='./test',
+             path='/tmp/test.local.conf')
+    lc = LocalConf(p.get('localrc'),
+                   p.get('local_conf'),
+                   p.get('services'),
+                   p.get('plugins'),
+                   p.get('base_dir'))
+    lc.write(p['path'])
+
+
+if __name__ == '__main__':
+    main()
