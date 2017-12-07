571cd9f16487ee47d4e2b17cce16c0f39f3ef7eb
lkuchlan <lkuchlan@redhat.com>
2017-12-07 16:26:11 +0200

    Add fixed_key parameter under key_manager section in nova.conf
    This change is necessary for boot instance from encrypted volume.
    
    Note: the bug [1] related to 16 bytes was resolved.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=1447297
    
    Change-Id: I777f9953323d3f0c9bf9a4dc2b2708920110c087

--- stack.sh
+++ stack.sh
@@ -1256,16 +1256,15 @@ if is_service_enabled g-reg; then
     done
 fi
 
-# Create a randomized default value for the key manager's fixed_key
-# NOTE(lyarwood): This is currently set to 36 as a workaround to the following
-# libvirt bug that incorrectly pads passphrases that are a multiple of 16 bytes
-# in length.
-# Unable to use LUKS passphrase that is exactly 16 bytes long
-# https://bugzilla.redhat.com/show_bug.cgi?id=1447297
+# Create a 16 bytes randomized default value for the key manager's fixed_key
+key=$(generate_hex_string 16)
 if is_service_enabled nova; then
-    key=$(generate_hex_string 36)
     iniset $NOVA_CONF key_manager fixed_key "$key"
     iniset $NOVA_CPU_CONF key_manager fixed_key "$key"
+fi
+
+if is_service_enabled cinder
+    iniset $CINDER_CONF key_manager fixed_key "$key"
 fi
 
 # Launch the nova-api and wait for it to answer before continuing
