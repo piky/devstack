29d42cbf25b347cedfe614aba5d93528bbe0087a
Dirk Mueller <dirk@dmllr.de>
2018-03-02 00:37:58 +0100

    WIP: Add an opensuse tumbleweed devstack job
    This adds the necessary fixes to pass a devstack run
    on opensuse tumbleweed.
    
    Depends-On: I21da4b5f851ec864dfb3cbf8ea6713bc12cda58f
    Change-Id: I2894482deef063fd02b0818c695a2ddbf6767039

--- tools/install_pip.sh
+++ tools/install_pip.sh
@@ -89,6 +89,9 @@ function install_get_pip {
             die $LINENO "Download of get-pip.py failed"
         touch $LOCAL_PIP.downloaded
     fi
+    cat /etc/pip.conf || :
+    # ugly hack until https://review.openstack.org/#/c/549181/ got merged
+    sudo sed -i -e "s,extra-index-url.*,," /etc/pip.conf
     sudo -H -E python $LOCAL_PIP -c $TOOLS_DIR/cap-pip.txt
     if python3_enabled; then
         sudo -H -E python${PYTHON3_VERSION} $LOCAL_PIP -c $TOOLS_DIR/cap-pip.txt
