4c0b69325ccff84ca8a720d80cf150cab26c9c6b
Clark Boylan <clark.boylan@gmail.com>
2018-04-04 14:02:30 -0700

    Add option to install everything in global venvs
    Since we are python3 only for openstack we create a single python3
    virtualenv to install all the packages into. This gives us the benefits
    of installing into a virtualenv while still ensuring coinstallability.
    This is a major change and will likely break many things.
    
    There are several reasons for this. The change that started this effort
    was pip stopped uninstalling packages which used distutils to generate
    their package installation. Many distro packages do this which meant
    that pip installed packages and distro packages could not coexist in the
    global install space. More recently git has made pip installing repos as
    root more difficult due to file ownership concerns.
    
    Currently the switch to the global venv is optional, but if we go down
    this path we should very quickly remove the old global installation
    method as it has only caused us problems.
    
    Major hurdles we have to get over are convincing rootwrap to trust
    binaries in the virtualenvs (so you'll notice we update rootwrap
    configs).
    
    Depends-On: https://review.openstack.org/559418
    Change-Id: If9bc7ba45522189d03f19b86cb681bb150ee2f25

--- stack.sh
+++ stack.sh
@@ -793,9 +793,13 @@ if [[ "$OFFLINE" != "True" ]]; then
     PYPI_ALTERNATIVE_URL=${PYPI_ALTERNATIVE_URL:-""} $TOP_DIR/tools/install_pip.sh
 fi
 
-# Do the ugly hacks for broken packages and distros
+# This will ensure that virtualenv is installed
 source $TOP_DIR/tools/fixup_stuff.sh
 fixup_all
+
+if [[ "$GLOBAL_VENV" == "True" ]] ; then
+    setup_devstack_virtualenv
+fi
 
 # Install subunit for the subunit output stream
 pip_install -U os-testr
