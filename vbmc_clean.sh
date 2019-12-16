7c6d1bc2d13e11b1af8d95b50ab39b43ea9f505a
Wang Jerry A <jerry.a.wang@intel.com>
2019-12-16 08:03:00 +0000

    Clean vbmc nodes to fix the issue that devstack failed because vbmc nodes were not cleaned. This would happen when run devstack from the second time.
    Change-Id: I640af8e998a443710cbe1b03b22ec656a7583473

---
+++ vbmc_clean.sh
@@ -0,0 +1,14 @@
+declare -i y=0
+str=""
+node_name=""
+str=$( vbmc list | grep node | awk '{print $2}' | wc -l)
+
+while true; do
+    if (($y==$str)); then break
+    fi
+    node_name="Clean node-$y"
+    echo $node_name
+    vbmc stop $node_name
+    vbmc delete $node_name
+    y=y+1
+done
