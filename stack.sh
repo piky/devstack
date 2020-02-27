32b8fc312c6338c3c9d6f7756a2f32956abf83f9
Federico Ressi <fressi@redhat.com>
2020-02-27 12:30:28 +0100

    [WIP] Parse bindep.txt file to install prerequisites packages
    Use bindep tool to get missing packages from files/bindep.txt
    file. It allows to use service names (nova, n-cpu, neutron,
    ...) as bindep profile name so that packages are installed
    only for required services.
    
    Change-Id: I91179240d99c383d0a4197204daa6f2c1a0ecade

--- stack.sh
+++ stack.sh
@@ -221,7 +221,7 @@ write_devstack_version
 
 # Warn users who aren't on an explicitly supported distro, but allow them to
 # override check and attempt installation with ``FORCE=yes ./stack``
-if [[ ! ${DISTRO} =~ (bionic|stretch|jessie|f30|opensuse-15.0|opensuse-15.1|opensuse-tumbleweed|rhel7) ]]; then
+if [[ ! ${DISTRO} =~ (bionic|stretch|jessie|f30|opensuse-15.0|opensuse-15.1|opensuse-tumbleweed|rhel7|rhel8) ]]; then
     echo "WARNING: this script has not been tested on $DISTRO"
     if [[ "$FORCE" != "yes" ]]; then
         die $LINENO "If you wish to run this script anyway run with FORCE=yes"
@@ -395,7 +395,7 @@ fi
 # to speed things up
 SKIP_EPEL_INSTALL=$(trueorfalse False SKIP_EPEL_INSTALL)
 
-if [[ $DISTRO == "rhel7" ]]; then
+if [[ $DISTRO == "rhel7" ]] || [[ $DISTRO == "rhel8" ]]; then
     # If we have /etc/ci/mirror_info.sh assume we're on a OpenStack CI
     # node, where EPEL is installed (but disabled) and already
     # pointing at our internal mirror
@@ -786,19 +786,15 @@ save_stackenv $LINENO
 # attempt to apply any constraints to pip installs.
 git_clone $REQUIREMENTS_REPO $REQUIREMENTS_DIR $REQUIREMENTS_BRANCH
 
-# Install package requirements
-# Source it so the entire environment is available
-echo_summary "Installing package prerequisites"
-source $TOP_DIR/tools/install_prereqs.sh
-
 # Configure an appropriate Python environment
 if [[ "$OFFLINE" != "True" ]]; then
     PYPI_ALTERNATIVE_URL=${PYPI_ALTERNATIVE_URL:-""} $TOP_DIR/tools/install_pip.sh
 fi
 
-# Do the ugly hacks for broken packages and distros
-source $TOP_DIR/tools/fixup_stuff.sh
-fixup_all
+# Install package requirements
+# Source it so the entire environment is available
+echo_summary "Installing package prerequisites"
+source $TOP_DIR/tools/install_prereqs.sh
 
 # Install subunit for the subunit output stream
 pip_install -U os-testr
@@ -816,12 +812,6 @@ fi
 
 # Install required infra support libraries
 install_infra
-
-# Install bindep
-$VIRTUALENV_CMD $DEST/bindep-venv
-# TODO(ianw) : optionally install from zuul checkout?
-$DEST/bindep-venv/bin/pip install bindep
-export BINDEP_CMD=${DEST}/bindep-venv/bin/bindep
 
 # Install packages as defined in plugin bindep.txt files
 pkgs="$( _get_plugin_bindep_packages )"
