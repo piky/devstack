436b70813aaab483c92a5c5cc80e782b765cf8a0
Jens Harbott <j.harbott@x-ion.de>
2020-06-11 05:51:26 +0000

    DNM: Use python3 pip module instead of pip binary
    Focal only provides a pip3 binary, no pip3.8. Instead of working around
    that with a symlink, use the module instead.
    
    Add version information output for this variant.
    
    Change-Id: I7af194ecc40e4d43c10ce067a661bb6ab4ca37d4

--- tools/install_pip.sh
+++ tools/install_pip.sh
@@ -5,7 +5,7 @@
 # Update pip and friends to a known common version
 
 # Assumptions:
-# - if USE_PYTHON3=True, PYTHON3_VERSION refers to a version already installed
+# - PYTHON3_VERSION refers to a version already installed
 
 set -o errexit
 
@@ -53,6 +53,8 @@ function get_versions {
     else
         echo "pip: Not Installed"
     fi
+    # Show python3 module version
+    python${PYTHON3_VERSION} -m pip --version
 }
 
 
@@ -125,11 +127,12 @@ function configure_pypi_alternative_url {
 # Show starting versions
 get_versions
 
-# Do pip
+if [[ -n $PYPI_ALTERNATIVE_URL ]]; then
+    configure_pypi_alternative_url
+fi
+
+# Just use system pkgs on Focal
 if [[ "$DISTRO" == focal ]]; then
-    install_package python3-pip python3.8-dev
-    sudo ln -sf pip3 /usr/bin/pip3.8
-    get_versions
     exit 0
 fi
 
@@ -148,10 +151,6 @@ if ! is_fedora  && ! is_suse; then
 fi
 
 install_get_pip
-
-if [[ -n $PYPI_ALTERNATIVE_URL ]]; then
-    configure_pypi_alternative_url
-fi
 
 set -x
 
