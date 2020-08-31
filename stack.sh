c3eb158e21f2b1539a4cdf757519dad362ba52cc
Ian Wienand <iwienand@redhat.com>
2020-08-31 10:37:39 +1000

    [dnm] testing suspected break of setuptools 50
    this pins setuptools
    
    Depends-On: https://review.opendev.org/748943
    Change-Id: I178d3493d62918837e8970d945a11d7cafaac0aa

--- stack.sh
+++ stack.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Testing
+
 # ``stack.sh`` is an opinionated OpenStack developer installation.  It
 # installs and configures various combinations of **Cinder**, **Glance**,
 # **Horizon**, **Keystone**, **Nova**, **Neutron**, and **Swift**
