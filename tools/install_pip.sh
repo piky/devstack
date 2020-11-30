f7cab562bcb33b2162d7661805ffcbef99d31a24
Elod Illes <elod.illes@est.tech>
2020-11-30 18:30:02 +0100

    WIP: Workaround for new pip 20.3 behavior
    This patch caps pip version during bootstrap to avoid the issue:
    
    "ERROR: Links are not allowed as constraints"
    
    A proper fix would be to adapt to new pip behavior.
    
    Change-Id: I1feed4573820436f91f8f654cc189fa3a21956fd

--- tools/install_pip.sh
+++ tools/install_pip.sh
@@ -91,7 +91,10 @@ function install_get_pip {
             die $LINENO "Download of get-pip.py failed"
         touch $LOCAL_PIP.downloaded
     fi
-    sudo -H -E python${PYTHON3_VERSION} $LOCAL_PIP
+    # TODO: remove the trailing pip constraint when a proper fix
+    # arrives for issue:
+    # ERROR: Links are not allowed as constraints
+    sudo -H -E python${PYTHON3_VERSION} $LOCAL_PIP 'pip<20.3'
 }
 
 
