f6034b8b78fabc910d15cac045e78ebc1c0e06a5
Federico Ressi <fressi@redhat.com>
2020-12-14 11:33:12 +0100

    Fix PowerTools repository name for CentOS 8.3
    This fixes below error when enabling PowerTools repo:
    
     $ sudo yum config-manager --set-enabled PowerTools
     Error: No matching repo to modify: PowerTools.
    
    Because of it DevStack fails later becaouse not
    being able to install libyaml-devel
    
    By replacing above command with below one the error
    disappears
    
     $ sudo yum config-manager --set-enabled powertools
    
    After it we are later able to install libyaml-devel
    package
    
    Change-Id: I92088f426ff3f5398978de61fdf53e120e403091

--- stack.sh
+++ stack.sh
@@ -352,9 +352,14 @@ if [[ $DISTRO == "rhel8" ]]; then
         sudo dnf config-manager --set-enabled epel
     fi
 
+
+    RHEL8_VERSION=$(awk '{split($N,v,"."); print v[2]}' /etc/redhat-release)
     # PowerTools repo provides libyaml-devel required by devstack itself and
     # EPEL packages assume that the PowerTools repository is enable.
-    sudo dnf config-manager --set-enabled PowerTools
+    # - Until CentOS 8.2 the name must be uppercase
+    # - Since CentOS 8.3 the name must be lowercase
+    sudo dnf config-manager --set-enabled powertools || \
+        sudo dnf config-manager --set-enabled PowerTools
 
     if [[ ${SKIP_EPEL_INSTALL} != True ]]; then
         _install_epel
