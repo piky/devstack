dd3a68fcc188293bb95e1dc1f4ce2d6c66c69b0e
Takashi Kajinami <tkajinam@redhat.com>
2021-07-30 17:46:58 +0900

    DNM: testing devstack-system-admin for bug 1938120
    Change-Id: I263c0d7fa3e7c7849b2dbea0fe53cb765c7914ec

--- stack.sh
+++ stack.sh
@@ -1051,9 +1051,9 @@ if is_service_enabled tls-proxy; then
     start_tls_proxy http-services '*' 443 $SERVICE_HOST 80
 fi
 
-# Write a clouds.yaml file and use the devstack-admin cloud
+# Write a clouds.yaml file and use the devstack-system-admin cloud
 write_clouds_yaml
-export OS_CLOUD=devstack-admin
+export OS_CLOUD=devstack-system-admin
 
 if is_service_enabled keystone; then
     echo_summary "Starting Keystone"
