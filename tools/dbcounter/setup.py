12c2e2ae4855159294073d838f6a41ea8e06f668
Dan Smith <dansmith@redhat.com>
2022-05-19 18:33:06 +0000

    Convert dbcounter to legacy setup.py
    For some reason on Jammy with fully-installed constraints, the pip
    "new way" of installing dbcounter fails with errors in its own
    library for unknown reasons. This is probably the only package we
    are installing that uses the "new way" so it breaks. There's no
    reason for this tiny plugin to use the "new way" especially since it
    requires more files and work, so just convert this back to setup.py
    to sidestep the problem.
    
    Change-Id: I1254a76854f43f74c62fad0dddb6c11d51367eae

---
+++ tools/dbcounter/setup.py
@@ -0,0 +1,13 @@
+import setuptools,sys
+setuptools.setup(
+    name='dbcounter',
+    author='Dan Smith',
+    author_email='dms@danplanet.com',
+    version='0.1',
+    descroption='A teeny tiny dbcounter plugin for use with devstack',
+    url='http://github.com/openstack/devstack',
+    license='Apache',
+    entry_points={
+        'sqlalchemy.plugins': ['dbcounter=dbcounter:LogCursorEventsPlugin'],
+    },
+)
