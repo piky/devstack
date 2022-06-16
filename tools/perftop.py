64df92fff396444ad85442bfa878fd7c2fa0f64f
Dan Smith <dansmith@redhat.com>
2022-06-16 09:18:31 -0700

    Add perftop.py tool for examining performance
    This adds a quick tool for dumping human-readable data out of
    a performance.json file.
    
    Change-Id: Idb1b63b0a1e14bb7dd98307057052e26357c36f5

---
+++ tools/perftop.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+# This takes a performance.json (or URL to one) and prints some
+# information about it.
+
+import argparse
+import json
+import requests
+import sys
+
+import prettytable
+
+
+def do_mem(data, top):
+    pt = prettytable.PrettyTable(['mem mb', 'service'])
+    services = [(e['MemoryCurrent'], e['service']) for e in data['services']
+                if isinstance(e['MemoryCurrent'], int)]
+    procs = [(e['rss'], e['cmd']) for e in data['processes']]
+    count = 0
+    total = 0
+    for mem, thing in reversed(sorted(services + procs)):
+        pt.add_row([mem >> 20, thing])
+        total += int(mem)
+        count += 1
+        if count >= top:
+            break
+    else:
+        # Only print total if we counted all the available
+        # data points.
+        pt.add_row([total >> 20, 'Total'])
+    print(pt)
+
+
+def do_db(data, top):
+    pt = prettytable.PrettyTable(['db', 'op', 'count'])
+    count = 0
+    for db in reversed(sorted(data['db'], key=lambda d: d['count'])):
+        pt.add_row([db['db'], db['op'], db['count']])
+        count += 1
+        if count >= top:
+            break
+    print(pt)
+
+
+def do_api(data, top):
+    pt = prettytable.PrettyTable(['service', 'op', 'count'])
+    count = 0
+    counts = []
+
+    # Flatten this out to one entry per service,op
+    for api in data['api']:
+        for k, v in api.items():
+            if '-' in k:
+                counts.append([api['service'], k, v])
+
+    for apicount in reversed(sorted(counts, key=lambda c: c[-1])):
+        pt.add_row(apicount)
+        count += 1
+        if count >= top:
+            break
+    print(pt)
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('--mem', action='store_true')
+    p.add_argument('--db', action='store_true')
+    p.add_argument('--api', action='store_true')
+    p.add_argument('--top', type=int, default=10)
+    p.add_argument('datafile')
+    args = p.parse_args()
+
+    if args.datafile.startswith('http'):
+        d = requests.get(args.datafile).json()
+    else:
+        d = json.loads(open(args.datafile).read())
+    if args.mem:
+        do_mem(d, args.top)
+    elif args.db:
+        do_db(d, args.top)
+    elif args.api:
+        do_api(d, args.top)
+    else:
+        print('Run with some operation!')
+        p.print_help()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
