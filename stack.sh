5795d307469005b7e100107ada6775c744e575ea
Slawek Kaplonski <skaplons@redhat.com>
2022-11-18 11:44:56 +0100

    Rename lib/neutron-legacy to lib/neutron
    Previously lib/neutron-legacy module was old module and the plan was to
    remove it and use new lib/neutron instead. But as new lib/neutron module
    was never really finished and used, it was decided to remove it from
    Devstack.
    This patch renames lib/neutron-legacy to be lib/neutron as it is defacto
    neutron module in Devstack.
    
    Closes-Bug: #1996748
    
    Change-Id: I3c42bc521bf8debf40700b559505cc30e2a38e1b

--- stack.sh
+++ stack.sh
@@ -605,7 +605,7 @@ source $TOP_DIR/lib/nova
 source $TOP_DIR/lib/placement
 source $TOP_DIR/lib/cinder
 source $TOP_DIR/lib/swift
-source $TOP_DIR/lib/neutron-legacy
+source $TOP_DIR/lib/neutron
 source $TOP_DIR/lib/ldap
 source $TOP_DIR/lib/dstat
 source $TOP_DIR/lib/tcpdump
